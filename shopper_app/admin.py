6f4e314ebd280c292c4a17901e09758809e13d0d
Omer Akram <om26er@gmail.com>
2016-11-02 12:22:59 +0500

    cleanup admin panel

--- shopper_app/admin.py
+++ shopper_app/admin.py
@@ -1,4 +1,6 @@
 from django.contrib import admin
+from django.contrib.auth.models import Group
+from rest_framework.authtoken.models import Token
 
 from shopper_app.models import User
 
@@ -8,3 +10,5 @@ class UserAdmin(admin.ModelAdmin):
         model = User
 
 admin.site.register(User, UserAdmin)
+admin.site.unregister(Group)
+admin.site.unregister(Token)
